b1b2cea86f30490dd6e6d847da018430520c0c76
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2015-04-16 17:41:11 -0600

    Flipped load order

--- lib/fakes-rspec.rb
+++ lib/fakes-rspec.rb
@@ -3,11 +3,11 @@ require 'rspec'
 require 'singleton'
 
 require 'fakes_rspec/block_criteria'
-require 'fakes_rspec/matcher'
 require 'fakes_rspec/nullo_specification'
 require 'fakes_rspec/occurrences'
 require 'fakes_rspec/received_occurrences_criteria'
 require 'fakes_rspec/received_criteria'
+require 'fakes_rspec/matcher'
 
 RSpec.configure do |config|
   config.after(:each) do
